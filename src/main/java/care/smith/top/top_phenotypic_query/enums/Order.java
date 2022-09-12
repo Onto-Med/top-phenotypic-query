4f46efd199fb391f109ccafcc8165b5f30ee7952
auciteli <auciteli@imise.uni-leipzig.de>
2022-09-12 17:25:06 +0200

    remove packages 'enums' and 'terminology'

--- src/main/java/care/smith/top/top_phenotypic_query/enums/Order.java
+++
@@ -1,7 +0,0 @@
-package care.smith.top.top_phenotypic_query.enums;
-
-public enum Order {
-  ASC,
-  DESC,
-  NONE
-}
--- src/main/java/care/smith/top/top_phenotypic_query/terminology/TerminologyService.java
+++
@@ -1,8 +0,0 @@
-package care.smith.top.top_phenotypic_query.terminology;
-
-public class TerminologyService {
-
-  public Terminology getTerminology(String id) {
-    return null;
-  }
-}
