2807bc7d5305c1f28d2b4eb985a36c5af7db3131
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-09-13 12:34:31 +0200

    Refactor h2 db connection in tests

--- src/test/java/care/smith/top/top_phenotypic_query/tests/SqlAdapterTest.java
+++ src/test/java/care/smith/top/top_phenotypic_query/tests/SqlAdapterTest.java
@@ -1,6 +1,8 @@
 package care.smith.top.top_phenotypic_query.tests;
 
 import care.smith.top.top_phenotypic_query.adapter.config.DataAdapterConfig;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URL;
@@ -9,27 +11,15 @@ import java.sql.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SqlAdapterTest {
+  static final String DB_URL = "jdbc:h2:mem:test-db;INIT=RUNSCRIPT FROM 'classpath:schema.sql'";
+  static final String DB_USER = "user";
+  static final String DB_PASS = "password";
 
-  static final String ADAPTER_CONFIG_FILE = "config/SQL_Adapter_Test.yml";
+  Connection con;
 
-  @Test
-  void testAdapterConnection() throws SQLException {
-    URL configFile =
-        Thread.currentThread().getContextClassLoader().getResource(ADAPTER_CONFIG_FILE);
-    assertNotNull(configFile);
-
-    DataAdapterConfig adapterConfig = DataAdapterConfig.getInstance(configFile.getPath());
-    assertNotNull(adapterConfig);
-
-    String url = adapterConfig.getConnectionAttribute("url");
-    String user = adapterConfig.getConnectionAttribute("user");
-    String password = adapterConfig.getConnectionAttribute("password");
-
-    assertNotNull(url);
-    assertNotNull(user);
-    assertNotNull(password);
-
-    Connection con = DriverManager.getConnection(url, user, password);
+  @BeforeEach
+  void setup() throws SQLException {
+    con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
     assertNotNull(con);
     assertTrue(con.isValid(0));
 
@@ -38,5 +28,23 @@ public class SqlAdapterTest {
 
     assertTrue(rs.next());
     assertEquals(rs.getInt(1), 0);
+  }
+
+  @AfterEach
+  void cleanup() throws SQLException {
+    con.close();
+    assertTrue(con.isClosed());
+  }
+
+  @Test
+  void testAdapterConnection() throws SQLException {
+    URL configFile =
+        Thread.currentThread().getContextClassLoader().getResource("config/SQL_Adapter_Test.yml");
+    assertNotNull(configFile);
+
+    DataAdapterConfig adapterConfig = DataAdapterConfig.getInstance(configFile.getPath());
+    assertNotNull(adapterConfig);
+
+    // TODO: init adapter and test query runs
   }
 }
