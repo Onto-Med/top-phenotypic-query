d6f81cf19da2e84310fe97c8a2e164eae287a55e
auciteli <auciteli@imise.uni-leipzig.de>
2022-10-05 19:01:59 +0200

    update test

--- src/test/java/care/smith/top/top_phenotypic_query/tests/intern/FullBMIAgeTestIntern.java
+++ src/test/java/care/smith/top/top_phenotypic_query/tests/intern/FullBMIAgeTestIntern.java
@@ -2,40 +2,40 @@ package care.smith.top.top_phenotypic_query.tests.intern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.math.BigDecimal;
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 
-import care.smith.top.model.Phenotype;
 import care.smith.top.model.Query;
 import care.smith.top.model.QueryCriterion;
-import care.smith.top.simple_onto_api.calculator.Calculator;
-import care.smith.top.simple_onto_api.calculator.expressions.ConstantExpression;
-import care.smith.top.simple_onto_api.calculator.expressions.FunctionExpression;
-import care.smith.top.simple_onto_api.calculator.expressions.MathExpression;
-import care.smith.top.simple_onto_api.calculator.expressions.VariableExpression;
-import care.smith.top.simple_onto_api.model.property.data.value.DecimalValue;
+import care.smith.top.top_phenotypic_query.adapter.DataAdapter;
 import care.smith.top.top_phenotypic_query.adapter.config.DataAdapterConfig;
+import care.smith.top.top_phenotypic_query.adapter.fhir.FHIRAdapter;
 import care.smith.top.top_phenotypic_query.adapter.sql.SQLAdapter;
-import care.smith.top.top_phenotypic_query.result.Phenotypes;
 import care.smith.top.top_phenotypic_query.result.ResultSet;
-import care.smith.top.top_phenotypic_query.result.Values;
-import care.smith.top.top_phenotypic_query.search.CompositeSearch;
 import care.smith.top.top_phenotypic_query.search.PhenotypeFinder;
 import care.smith.top.top_phenotypic_query.tests.AbstractTest;
-import care.smith.top.top_phenotypic_query.ucum.UCUM;
 
 public class FullBMIAgeTestIntern extends AbstractTest {
 
   public static void main(String[] args) throws SQLException {
-    //    print();
-    //    test();
-    //    System.exit(0);
+    test(getSQLAdapter());
+  }
 
+  private static FHIRAdapter getFHIRAdapter() {
+    URL configFile =
+        Thread.currentThread().getContextClassLoader().getResource("config/FHIR_Adapter_Test.yml");
+    return new FHIRAdapter(DataAdapterConfig.getInstance(configFile.getPath()));
+  }
+
+  private static SQLAdapter getSQLAdapter() {
+    return new SQLAdapter(DataAdapterConfig.getInstance("test_files/SQL_Adapter_Test_intern.yml"));
+  }
+
+  private static void test(DataAdapter adapter) throws SQLException {
     QueryCriterion cri1 =
         new QueryCriterion()
             .inclusion(true)
@@ -45,10 +45,6 @@ public class FullBMIAgeTestIntern extends AbstractTest {
     QueryCriterion cri2 = new QueryCriterion().inclusion(true).subjectId(female.getId());
     QueryCriterion cri3 = new QueryCriterion().inclusion(true).subjectId(old.getId());
     Query query = new Query().addCriteriaItem(cri1).addCriteriaItem(cri2).addCriteriaItem(cri3);
-
-    DataAdapterConfig config =
-        DataAdapterConfig.getInstance("test_files/SQL_Adapter_Test_intern.yml");
-    SQLAdapter adapter = new SQLAdapter(config);
 
     PhenotypeFinder pf = new PhenotypeFinder(query, phenotypes, adapter);
     ResultSet rs = pf.execute();
@@ -85,12 +81,10 @@ public class FullBMIAgeTestIntern extends AbstractTest {
   }
 
   private static List<String> getExpectedSubjectIds() throws SQLException {
-    DataAdapterConfig sqlConfig =
-        DataAdapterConfig.getInstance("test_files/SQL_Adapter_Test_intern.yml");
-    SQLAdapter sqlAdapter = new SQLAdapter(sqlConfig);
     java.sql.ResultSet rs =
-        sqlAdapter.executeQuery(
-            "SELECT s.subject_id, birth_date, sex, assessment_id, created_at, height, weight, DATE_PART('year', AGE(CURRENT_DATE, birth_date)) years, (weight/((height/100)^2)) bmi FROM subject s, (select distinct on (subject_id) subject_id, assessment_id, created_at, height, weight from assessment1 where height is not null and weight is not null and created_at >= '2000-01-01'::date and created_at < '2001-01-01'::date order by subject_id, created_at desc) a WHERE s.subject_id = a.subject_id AND sex = 'female' AND DATE_PART('year', AGE(CURRENT_DATE, birth_date)) > 18 AND (weight/((height/100)^2)) >= 27 AND (weight/((height/100)^2)) < 30 ORDER BY s.subject_id");
+        getSQLAdapter()
+            .executeQuery(
+                "SELECT s.subject_id, birth_date, sex, assessment_id, created_at, height, weight, DATE_PART('year', AGE(CURRENT_DATE, birth_date)) years, (weight/((height/100)^2)) bmi FROM subject s, (select distinct on (subject_id) subject_id, assessment_id, created_at, height, weight from assessment1 where height is not null and weight is not null and created_at >= '2000-01-01'::date and created_at < '2001-01-01'::date order by subject_id, created_at desc) a WHERE s.subject_id = a.subject_id AND sex = 'female' AND DATE_PART('year', AGE(CURRENT_DATE, birth_date)) > 18 AND (weight/((height/100)^2)) >= 27 AND (weight/((height/100)^2)) < 30 ORDER BY s.subject_id");
     //    SQLAdapter.print(rs);
 
     List<String> ids = new ArrayList<>();
@@ -99,72 +93,74 @@ public class FullBMIAgeTestIntern extends AbstractTest {
     return ids;
   }
 
-  private static void print() throws SQLException {
-    DataAdapterConfig sqlConfig =
-        DataAdapterConfig.getInstance("test_files/SQL_Adapter_Test_intern.yml");
-    SQLAdapter sqlAdapter = new SQLAdapter(sqlConfig);
-    java.sql.ResultSet rs =
-        sqlAdapter.executeQuery(
-            "SELECT s.subject_id, birth_date, sex, assessment_id, created_at, height, weight FROM subject s, assessment1 a WHERE s.subject_id = a.subject_id AND s.subject_id = 3269 ORDER BY created_at");
-    SQLAdapter.print(rs);
-  }
-
-  public static void testBMI() {
-    Calculator c = new Calculator();
-    MathExpression e =
-        new FunctionExpression("divide")
-            .arg(new VariableExpression("m"))
-            .arg(
-                new FunctionExpression("power")
-                    .arg(new VariableExpression("l"))
-                    .arg(new ConstantExpression(new DecimalValue(2))));
-
-    c.setVariable("m", 76.6057689700064);
-    c.setVariable("l", UCUM.convert(new BigDecimal("167.617921540792"), "cm", "m"));
-
-    System.out.println(c.calculate(e).getValueDecimal());
-  }
-
-  private static void test() {
-    Values weightVals1 = new Values("Weight");
-    weightVals1.setDecimalValues(getDTR(2000), new DecimalValue(76.6057689700064));
-    Values heightVals1 = new Values("Height");
-    heightVals1.setDecimalValues(getDTR(2000), new DecimalValue(1.67617921540792));
-    Values ageVals1 = new Values("Age");
-    ageVals1.setDecimalValues(getDTR(2000), new DecimalValue(57));
-
-    Phenotypes phes1 = new Phenotypes("Subject1");
-    phes1.setValues(weightVals1, heightVals1, ageVals1);
-
-    ResultSet rs = new ResultSet();
-    rs.setPhenotypes(phes1);
-
-    QueryCriterion cri =
-        new QueryCriterion()
-            .inclusion(true)
-            .defaultAggregationFunctionId(defAgrFunc.getId())
-            .subjectId(overWeight.getId())
-            .dateTimeRestriction(getDTR(2000));
-
-    Map<String, Phenotype> phenotypes =
-        getPhenotypeMap(
-            weight,
-            height,
-            age,
-            young,
-            old,
-            bmi,
-            bmi19_25,
-            bmi19_27,
-            bmi25_30,
-            bmi27_30,
-            finding,
-            overWeight);
-
-    System.out.println(rs);
-
-    CompositeSearch search = new CompositeSearch(null, cri, rs, phenotypes);
-    ResultSet finalRS = search.execute();
-    System.out.println(finalRS);
-  }
+  //  private static void print() throws SQLException {
+  //    DataAdapterConfig sqlConfig =
+  //        DataAdapterConfig.getInstance("test_files/SQL_Adapter_Test_intern.yml");
+  //    SQLAdapter sqlAdapter = new SQLAdapter(sqlConfig);
+  //    java.sql.ResultSet rs =
+  //        sqlAdapter.executeQuery(
+  //            "SELECT s.subject_id, birth_date, sex, assessment_id, created_at, height, weight
+  // FROM subject s, assessment1 a WHERE s.subject_id = a.subject_id AND s.subject_id = 3269 ORDER
+  // BY created_at");
+  //    SQLAdapter.print(rs);
+  //  }
+  //
+  //  public static void testBMI() {
+  //    Calculator c = new Calculator();
+  //    MathExpression e =
+  //        new FunctionExpression("divide")
+  //            .arg(new VariableExpression("m"))
+  //            .arg(
+  //                new FunctionExpression("power")
+  //                    .arg(new VariableExpression("l"))
+  //                    .arg(new ConstantExpression(new DecimalValue(2))));
+  //
+  //    c.setVariable("m", 76.6057689700064);
+  //    c.setVariable("l", UCUM.convert(new BigDecimal("167.617921540792"), "cm", "m"));
+  //
+  //    System.out.println(c.calculate(e).getValueDecimal());
+  //  }
+  //
+  //  private static void test() {
+  //    Values weightVals1 = new Values("Weight");
+  //    weightVals1.setDecimalValues(getDTR(2000), new DecimalValue(76.6057689700064));
+  //    Values heightVals1 = new Values("Height");
+  //    heightVals1.setDecimalValues(getDTR(2000), new DecimalValue(1.67617921540792));
+  //    Values ageVals1 = new Values("Age");
+  //    ageVals1.setDecimalValues(getDTR(2000), new DecimalValue(57));
+  //
+  //    Phenotypes phes1 = new Phenotypes("Subject1");
+  //    phes1.setValues(weightVals1, heightVals1, ageVals1);
+  //
+  //    ResultSet rs = new ResultSet();
+  //    rs.setPhenotypes(phes1);
+  //
+  //    QueryCriterion cri =
+  //        new QueryCriterion()
+  //            .inclusion(true)
+  //            .defaultAggregationFunctionId(defAgrFunc.getId())
+  //            .subjectId(overWeight.getId())
+  //            .dateTimeRestriction(getDTR(2000));
+  //
+  //    Map<String, Phenotype> phenotypes =
+  //        getPhenotypeMap(
+  //            weight,
+  //            height,
+  //            age,
+  //            young,
+  //            old,
+  //            bmi,
+  //            bmi19_25,
+  //            bmi19_27,
+  //            bmi25_30,
+  //            bmi27_30,
+  //            finding,
+  //            overWeight);
+  //
+  //    System.out.println(rs);
+  //
+  //    CompositeSearch search = new CompositeSearch(null, cri, rs, phenotypes);
+  //    ResultSet finalRS = search.execute();
+  //    System.out.println(finalRS);
+  //  }
 }
