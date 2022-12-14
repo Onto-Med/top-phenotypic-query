ca73df719b9c129884090415421e50ce08e508e8
auciteli <auciteli@imise.uni-leipzig.de>
2022-12-14 16:48:21 +0100

    update PhenotypeExporter

--- src/main/java/care/smith/top/top_phenotypic_query/converter/PhenotypeExporter.java
+++ src/main/java/care/smith/top/top_phenotypic_query/converter/PhenotypeExporter.java
@@ -1,13 +1,13 @@
 package care.smith.top.top_phenotypic_query.converter;
 
 import java.io.File;
-import java.io.InputStream;
+import java.io.OutputStream;
 
 import care.smith.top.top_phenotypic_query.util.PhenotypeList;
 
 public interface PhenotypeExporter {
 
-  public void write(PhenotypeList phenotypes, InputStream stream);
+  public void write(PhenotypeList phenotypes, OutputStream stream);
 
   public void write(PhenotypeList phenotypes, File file);
 }
