8d09c9db80b866eac10876dfad8078cbbe488c35
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2023-01-30 16:26:20 +0100

    Add `getType()` to function entities and reformat code

---
+++ src/test/java/care/smith/top/top_phenotypic_query/c2reasoner/functions/FunctionEntityTest.java
@@ -0,0 +1,16 @@
+package care.smith.top.top_phenotypic_query.c2reasoner.functions;
+
+import care.smith.top.top_phenotypic_query.c2reasoner.functions.arithmetic.Add;
+import care.smith.top.top_phenotypic_query.c2reasoner.functions.date_time.DiffDays;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FunctionEntityTest {
+
+  @Test
+  void getType() {
+    assertEquals("arithmetic", Add.get().getType());
+    assertEquals("date_time", DiffDays.get().getType());
+  }
+}
