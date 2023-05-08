6c4e8b31c6cf51955c9ed5ac3575411d5b041f23
auciteli <auciteli@imise.uni-leipzig.de>
2023-05-08 17:08:29 +0200

    add test of default FHIR adapter configuration

---
+++ src/test/java/care/smith/top/top_phenotypic_query/tests/intern/DefaultFHIRDelirTestIntern.java
@@ -0,0 +1,75 @@
+package care.smith.top.top_phenotypic_query.tests.intern;
+
+import org.junit.jupiter.api.Test;
+
+import care.smith.top.top_phenotypic_query.tests.DelirTest;
+
+public class DefaultFHIRDelirTestIntern extends DelirTest {
+
+  private static final String CONFIG = "config/Default_Delir_FHIR_Adapter_Test.yml";
+
+  @Test
+  public void testFHIRAlgA() throws InstantiationException {
+    testAlgA(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRExpIcd() throws InstantiationException {
+    testExpIcd(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRAlgB() throws InstantiationException {
+    testAlgB(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIR2of3() throws InstantiationException {
+    test2of3(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRExtAlg() throws InstantiationException {
+    testExtAlg(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRExtAlgWithDate() throws InstantiationException {
+    testExtAlgWithDate(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRNotExtAlg() throws InstantiationException {
+    testNotExtAlg(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRExtAlgExcAlgB() throws InstantiationException {
+    testExtAlgExcAlgB(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRExtAlgAndFemale() throws InstantiationException {
+    testExtAlgAndFemale(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRExtAlgExcFemale() throws InstantiationException {
+    testExtAlgExcFemale(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRAntiPsyAndOp() throws InstantiationException {
+    testAntiPsyAndOp(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRImpIcdAndAntiPsyOrOp() throws InstantiationException {
+    testImpIcdAndAntiPsyOrOp(CONFIG, true);
+  }
+
+  @Test
+  public void testFHIRCognitAnd2of3() throws InstantiationException {
+    testCognitAnd2of3(CONFIG, true);
+  }
+}
