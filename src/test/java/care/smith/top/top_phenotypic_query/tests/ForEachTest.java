3172aeb02170773465620c4aeb8fc5af965b1a35
auciteli <auciteli@imise.uni-leipzig.de>
2023-08-01 18:30:08 +0200

    update ForEachTest

--- src/test/java/care/smith/top/top_phenotypic_query/tests/ForEachTest.java
+++ src/test/java/care/smith/top/top_phenotypic_query/tests/ForEachTest.java
@@ -1,6 +1,9 @@
 package care.smith.top.top_phenotypic_query.tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +18,7 @@ import care.smith.top.top_phenotypic_query.c2reasoner.functions.comparison.Gt;
 import care.smith.top.top_phenotypic_query.c2reasoner.functions.set.RefValues;
 import care.smith.top.top_phenotypic_query.result.SubjectPhenotypes;
 import care.smith.top.top_phenotypic_query.util.Entities;
+import care.smith.top.top_phenotypic_query.util.Expressions;
 import care.smith.top.top_phenotypic_query.util.builder.Exp;
 import care.smith.top.top_phenotypic_query.util.builder.Phe;
 import care.smith.top.top_phenotypic_query.util.builder.Val;
@@ -33,8 +37,7 @@ public class ForEachTest {
           .expression(Divide.of(Exp.of(crea), Min.of(RefValues.of(Exp.of(crea), Exp.of(7)))))
           .get();
 
-  private static Phenotype check =
-      new Phe("check").expression(Gt.of(Exp.of(ref), Exp.of(1.5))).get();
+  private static Phenotype check = new Phe("check").expression(Gt.of(Exp.of(ref), Exp.of(2))).get();
 
   private static Phenotype forEach = new Phe("forEach").expression(ForEach.of(crea, check)).get();
 
@@ -44,30 +47,30 @@ public class ForEachTest {
     LocalDateTime now = LocalDateTime.now();
 
     vals.addValue("crea", null, Val.of(10, now.minusDays(15)));
-    vals.addValue("crea", null, Val.of(11, now.minusDays(14)));
-    vals.addValue("crea", null, Val.of(12, now.minusDays(13)));
-    vals.addValue("crea", null, Val.of(13, now.minusDays(12)));
-    vals.addValue("crea", null, Val.of(14, now.minusDays(11)));
-    vals.addValue("crea", null, Val.of(15, now.minusDays(10)));
-    vals.addValue("crea", null, Val.of(16, now.minusDays(9)));
-    vals.addValue("crea", null, Val.of(17, now.minusDays(8)));
-    vals.addValue("crea", null, Val.of(18, now.minusDays(7)));
-    vals.addValue("crea", null, Val.of(19, now.minusDays(6)));
-    vals.addValue("crea", null, Val.of(20, now.minusDays(5)));
-    vals.addValue("crea", null, Val.of(21, now.minusDays(4)));
-    vals.addValue("crea", null, Val.of(22, now.minusDays(3)));
-    vals.addValue("crea", null, Val.of(23, now.minusDays(2)));
-    vals.addValue("crea", null, Val.of(24, now.minusDays(1)));
+    vals.addValue("crea", null, Val.of(12, now.minusDays(14)));
+    vals.addValue("crea", null, Val.of(14, now.minusDays(13)));
+    vals.addValue("crea", null, Val.of(16, now.minusDays(12)));
+    vals.addValue("crea", null, Val.of(18, now.minusDays(11)));
+    vals.addValue("crea", null, Val.of(20, now.minusDays(10)));
+    vals.addValue("crea", null, Val.of(22, now.minusDays(9)));
+    vals.addValue("crea", null, Val.of(24, now.minusDays(8)));
+    vals.addValue("crea", null, Val.of(26, now.minusDays(7)));
+    vals.addValue("crea", null, Val.of(28, now.minusDays(6)));
+    vals.addValue("crea", null, Val.of(30, now.minusDays(5)));
+    vals.addValue("crea", null, Val.of(32, now.minusDays(4)));
+    vals.addValue("crea", null, Val.of(34, now.minusDays(3)));
+    vals.addValue("crea", null, Val.of(36, now.minusDays(2)));
+    vals.addValue("crea", null, Val.of(38, now.minusDays(1)));
 
     Entities phens = Entities.of(crea, ref, check, forEach);
 
     C2R c = new C2R().phenotypes(phens).values(vals);
     Expression res = c.calculate(forEach);
 
-    //    System.out.println(res);
-
-    //    assertEquals(
-    //        List.of(BigDecimal.valueOf(106), BigDecimal.valueOf(110), BigDecimal.valueOf(108)),
-    //        Expressions.getNumberValues(c.calculate(p)));
+    assertEquals(
+        List.of(
+            false, false, false, false, false, true, true, true, false, false, false, false, false,
+            false),
+        Expressions.getBooleanValues(res));
   }
 }
