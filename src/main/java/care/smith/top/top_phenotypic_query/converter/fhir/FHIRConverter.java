f83a2759b51057470d6f25dc26d7637a4301f26a
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-11-13 13:32:44 +0100

    Add comments after discussion with AU, improve conversion

--- src/main/java/care/smith/top/top_phenotypic_query/converter/fhir/FHIRConverter.java
+++ src/main/java/care/smith/top/top_phenotypic_query/converter/fhir/FHIRConverter.java
@@ -6,13 +6,18 @@ import care.smith.top.model.Value;
 import care.smith.top.top_phenotypic_query.result.PhenotypeValues;
 import care.smith.top.top_phenotypic_query.result.ResultSet;
 import care.smith.top.top_phenotypic_query.result.SubjectPhenotypes;
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Collections;
 import java.util.List;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Quantity;
 import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.StringType;
 
 public class FHIRConverter {
 
@@ -20,8 +25,19 @@ public class FHIRConverter {
   public static void write(ResultSet rs, OutputStream out) {
     Bundle bundle = new Bundle();
 
+    // nicht nur observations ausgeben
+    // bundle aus questioner responses
+    // questionaire überlegen, generische darstellen
+    // pro Datensatz/Patient gibt es eine Questionaire Response (ausgefülltes Formular)
+    // wie mächtig sind Tools, können sie so etwas darstellen?
+    // Resultset muss übersetzt werden
+    // ob es da Suche, Filter und ähnliches
+
     for (SubjectPhenotypes sbjPhes : rs.values()) {
       Patient p = new Patient();
+      var name = new HumanName();
+      name.setGiven(Collections.singletonList(new StringType(sbjPhes.getSubjectId())));
+      p.setName(Collections.singletonList(name));
       // todo: add patient to bundle?
       for (PhenotypeValues pheVals : sbjPhes.values()) {
 
@@ -31,6 +47,11 @@ public class FHIRConverter {
             // pheVals.g
             // o.setCode(new CodeableConcept()); //
             o.setSubject(new Reference(p));
+            var q = new Quantity();
+            // q.setId(vals.getPhenotypeName().toString());
+            // q.setValue(val.get);
+            o.setValue(q);
+
             var component = bundle.addEntry();
             component.setResource(o);
             // sbjPhes.
@@ -48,5 +69,11 @@ public class FHIRConverter {
         }
       }
     }
+  }
+
+  public static String toString(ResultSet rs) {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    write(rs, out);
+    return out.toString();
   }
 }
--- src/test/java/care/smith/top/top_phenotypic_query/tests/FHIRConverterTest.java
+++ src/test/java/care/smith/top/top_phenotypic_query/tests/FHIRConverterTest.java
@@ -2,18 +2,15 @@ package care.smith.top.top_phenotypic_query.tests;
 
 import care.smith.top.top_phenotypic_query.converter.fhir.FHIRConverter;
 import care.smith.top.top_phenotypic_query.result.ResultSet;
-import java.io.ByteArrayOutputStream;
 import org.junit.jupiter.api.Test;
 
 class FHIRConverterTest {
 
   @Test
   void test() {
-    var out = new ByteArrayOutputStream();
     ResultSet testSet = BMIAgeTest.getResultSet();
-    FHIRConverter.write(testSet, out);
-    String s = out.toString();
-
-    System.out.println(out.toString());
+    System.out.println(testSet);
+    String s = FHIRConverter.toString(testSet);
+    System.out.println(s);
   }
 }
