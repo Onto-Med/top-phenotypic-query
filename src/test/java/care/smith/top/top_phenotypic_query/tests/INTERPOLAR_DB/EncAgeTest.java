445fb3faa8ab68873bdae5ff0718acdda64de414
Ralph Schaefermeier <ralph.schafermeier@gmail.com>
2024-09-17 15:08:34 +0200

    Fix shifting age bug in test code

--- src/test/java/care/smith/top/top_phenotypic_query/tests/INTERPOLAR_DB/EncAgeTest.java
+++ src/test/java/care/smith/top/top_phenotypic_query/tests/INTERPOLAR_DB/EncAgeTest.java
@@ -2,23 +2,31 @@ package care.smith.top.top_phenotypic_query.tests.INTERPOLAR_DB;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import care.smith.top.model.DateTimeValue;
 import care.smith.top.model.ItemType;
 import care.smith.top.model.Phenotype;
 import care.smith.top.top_phenotypic_query.c2reasoner.functions.encounter.EncAge;
 import care.smith.top.top_phenotypic_query.result.ResultSet;
 import care.smith.top.top_phenotypic_query.util.builder.Phe;
 import care.smith.top.top_phenotypic_query.util.builder.Que;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
 import org.junit.jupiter.api.Test;
 
 public class EncAgeTest {
 
   private static final String CONFIG = "config/INTERPOLAR_DB_Adapter.yml";
 
-  private static Phenotype bd =
+  private static final Phenotype bd =
       new Phe("birth date").itemType(ItemType.SUBJECT_BIRTH_DATE).dateTime().get();
-  private static Phenotype age = new Phe("age").itemType(ItemType.SUBJECT_AGE).number().get();
-  private static Phenotype enc = new Phe("encounter").itemType(ItemType.ENCOUNTER).string().get();
-  private static Phenotype encAge = new Phe("encounter age").expression(EncAge.of(bd, enc)).get();
+  private static final Phenotype age = new Phe("age").itemType(ItemType.SUBJECT_AGE).number().get();
+  private static final Phenotype enc =
+      new Phe("encounter").itemType(ItemType.ENCOUNTER).string().get();
+  private static final Phenotype encAge =
+      new Phe("encounter age").expression(EncAge.of(bd, enc)).get();
 
   @Test
   void test() throws InstantiationException {
@@ -33,14 +41,31 @@ public class EncAgeTest {
 
     System.out.println(rs);
 
-    assertEquals(84, rs.getNumberValue("11", "age", null).intValue());
+    LocalDate birthdate11 = LocalDate.of(1940, Month.JANUARY, 1);
+    LocalDate birthdate21 = LocalDate.of(1980, Month.JULY, 1);
+
+    assertEquals(
+        birthdate11,
+        ((DateTimeValue) rs.getValues("11", "birth date", null).getFirst())
+            .getValue()
+            .toLocalDate());
+    assertEquals(
+        birthdate21,
+        ((DateTimeValue) rs.getValues("21", "birth date", null).getFirst())
+            .getValue()
+            .toLocalDate());
+
+    int age11 = Period.between(birthdate11, LocalDate.now()).getYears();
+    int age21 = Period.between(birthdate21, LocalDate.now()).getYears();
+
+    assertEquals(age11, rs.getNumberValue("11", "age", null).intValue());
     assertEquals(60, rs.getNumberValue("11", "encounter age", null).intValue());
-    assertEquals(84, rs.getNumberValue("12", "age", null).intValue());
+    assertEquals(age11, rs.getNumberValue("12", "age", null).intValue());
     assertEquals(70, rs.getNumberValue("12", "encounter age", null).intValue());
 
-    assertEquals(43, rs.getNumberValue("21", "age", null).intValue());
+    assertEquals(age21, rs.getNumberValue("21", "age", null).intValue());
     assertEquals(9, rs.getNumberValue("21", "encounter age", null).intValue());
-    assertEquals(43, rs.getNumberValue("22", "age", null).intValue());
+    assertEquals(age21, rs.getNumberValue("22", "age", null).intValue());
     assertEquals(39, rs.getNumberValue("22", "encounter age", null).intValue());
   }
 }
