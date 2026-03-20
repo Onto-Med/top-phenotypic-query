df303f57697427049741cf6d68b8289822c8676f
ChristophB <christoph.beger@medizin.uni-leipzig.de>
2026-03-20 12:53:26 +0100

    Add custom reporting to list-phenotype-classes command

--- src/main/java/care/smith/top/top_phenotypic_query/analysis/ListPhenotypeClasses.java
+++ src/main/java/care/smith/top/top_phenotypic_query/analysis/ListPhenotypeClasses.java
@@ -1,7 +1,11 @@
 package care.smith.top.top_phenotypic_query.analysis;
 
+import com.opencsv.CSVWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import picocli.CommandLine.Command;
 
@@ -13,7 +17,24 @@ public class ListPhenotypeClasses extends Analysis {
 
   @Override
   public Optional<List<AnalysisReport>> analyse(File queryResultFile) {
-    System.out.println(loadMetadata(queryResultFile));
+    if (outputFile.isPresent()) {
+      try (CSVWriter writer = new CSVWriter(new FileWriter(outputFile.get()))) {
+        loadMetadata(queryResultFile).stream()
+            .map(
+                phe ->
+                    new String[] {
+                      phe.getId(),
+                      Objects.requireNonNullElse(phe.getEntityType(), "").toString(),
+                      Objects.requireNonNullElse(phe.getDataType(), "").toString(),
+                      phe.getUnit()
+                    })
+            .forEach(line -> writer.writeNext(line));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } else {
+      System.out.println(loadMetadata(queryResultFile));
+    }
     return Optional.empty();
   }
 }
